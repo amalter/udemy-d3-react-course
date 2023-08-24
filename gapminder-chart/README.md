acca3c6b29df1609c8a828c833fd356dd7b08922
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2023-08-23 17:17:44 -0700

    add gapminder-chart project

---
+++ gapminder-chart/README.md
@@ -0,0 +1,7 @@
+# Gapminder chart
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). And D3.js
+
+## Description
+
+Practice project that uses REACT and D3 to create scatter plot visualizatin of GDP and Life expectancy by country.
---
+++ gapminder-chart/src/Circles.js
@@ -0,0 +1,58 @@
+import { select } from "d3";
+import { useEffect, useRef } from "react";
+import { drawChart } from "./drawChart";
+import data from "./gapminder_data.json";
+import "./index.css";
+
+// https://observablehq.com/@uwdata/introduction-to-d3-part-2
+
+const margin = { left: 50, right: 20, top: 30, bottom: 50 };
+
+function Circles({ width, height, data, year, colorScale, selectedContinent }) {
+  const chartData = data.filter((d) => d.year === year);
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const SVG = select(svgRef.current);
+    drawChart(
+      SVG,
+      chartData,
+      data,
+      height,
+      width,
+      margin,
+      colorScale,
+      selectedContinent
+    );
+  }, [chartData, data, height, width, colorScale, selectedContinent]);
+  // this just attaches circles to the DOM - it doesn't actually set their size, color, or position
+  return (
+    <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}>
+      <line stroke="lightgrey" strokeDasharray={"10 2"} className="life-avg" />
+      <line stroke="lightgrey" strokeDasharray={"10 2"} className="gdp-avg" />
+      {chartData.map((d) => (
+        <circle key={d.country + d.year} fill="#fff" />
+      ))}
+      <text
+        fontSize="48px"
+        x={width - margin.right - 150}
+        y={height - margin.bottom - 50}
+      >
+        {year}
+      </text>
+      <text y={height - 20} x={20} fill="grey">
+        {" "}
+        GDP per Capita
+      </text>
+      <text
+        transform={`translate(${20}, ${margin.top + 100}) rotate(-90)`}
+        fill="grey"
+      >
+        Life Expectancy
+      </text>
+    </svg>
+  );
+}
+
+export default Circles;
